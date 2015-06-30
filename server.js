a08c4e3c7359e9053cc26f73bdf194cbe9f2ee88
nezz <imtaehyun@me.com>
2015-06-30 16:42:47 +0900

    bot hook에 parse에서 user 조회하는 부분 추가

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
-var Hapi = require('hapi');
+var Hapi = require('hapi'),
+    Parse = require('node-parse-api').Parse;
 
-var host = process.env.HOST || '0.0.0.0',
-    port = process.env.PORT || 3000;
+var db = new Parse({
+    app_id: 'CRwSkDGmsmEr1vYdWkjj1s0jkQKUISTBWwu8WFN5',
+    api_key: 'vpJD4yHcMwuWR8jP4evCtt6E6CyVyV8EXjBwFoVz'
+});
+
 var server = new Hapi.Server();
 server.connection({
-    host: host,
-    port: port
+    host: process.env.HOST || '0.0.0.0',
+    port: process.env.PORT || 3000
 });
 
 server.route({
@@ -13,6 +17,13 @@ server.route({
     path: '/bot/hook',
     handler: function(request, reply) {
         console.log(request.payload);
+        var update = request.payload;
+        if (update.message.text === '/start') {
+            db.find('User', { username: update.message.from.id }, function(err, response) {
+                if (err) console.error('err: ' + err);
+                else console.log(response);
+            });
+        }
         reply('ok');
     }
 });
