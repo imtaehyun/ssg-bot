bdbfcc575d86bd41f0ae87036da4883eedf7da83
nezz <imtaehyun@me.com>
2015-06-30 17:07:43 +0900

    bot hook 기능 추가 /start가 왔을때 user확인하여 parse db에 추가하는 기능

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ var Hapi = require('hapi'),
     Parse = require('node-parse-api').Parse;
 
 var db = new Parse({
-    app_id: 'CRwSkDGmsmEr1vYdWkjj1s0jkQKUISTBWwu8WFN5',
-    api_key: 'vpJD4yHcMwuWR8jP4evCtt6E6CyVyV8EXjBwFoVz'
+    app_id: process.env.PARSE_APP_ID,
+    api_key: process.env.PARSE_API_KEY
 });
 
 var server = new Hapi.Server();
@@ -20,11 +20,35 @@ server.route({
         var update = request.payload;
         if (update.message.text === '/start') {
             db.find('User', { username: update.message.from.id }, function(err, response) {
-                if (err) console.error('err: ' + err);
-                else console.log(response);
+                if (err) {
+                    console.error('err: ' + err);
+                    reply('err');
+                } else {
+                    if (response.results.length == 0) {
+                        db.insert('User', {
+                            telegramId: update.message.from.id,
+                            username: update.message.from.username,
+                            firstName: update.message.from.first_name,
+                            lastName: update.message.from.last_name
+                        }, function(err, response) {
+                            if (err) {
+                                console.log('err insert user: ' + JSON.stringify(err));
+                                reply('err insert user: ' + err);
+                            } else {
+                                console.log(response);
+                                console.log('add user: ' + JSON.stringify(update.message.from));
+                                reply('add user: ' + JSON.stringify(update.message.from));
+                            }
+                        });
+                    } else {
+                        reply('exist user');
+                    }
+                }
             });
+        } else {
+            reply('ok');
         }
-        reply('ok');
+
     }
 });
 
