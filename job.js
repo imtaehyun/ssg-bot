766ea992e206280b828c8c06b667d6fe485f188f
nezz <imtaehyun@me.com>
2015-07-02 01:12:36 +0900

    Edit Job

--- job.js
+++ job.js
@@ -1,5 +1,54 @@
-var CronJob = require('cron').CronJob;
+var _ = require('underscore'),
+    CronJob = require('cron').CronJob,
+    SSG = require('./ssg')
+    db = require('./database')
+    ;
 
-new CronJob('*/30 * * * * *', function() {
-    console.log('test');
-}, null, true, null, null);
+var Job = {
+    start: function() {
+        this.getTodayCardPromo().start();
+    },
+
+    getTodayCardPromo: function() {
+        return new CronJob({
+            cronTime: '00 53 00 * * *',
+            onTick: function() {
+
+                console.log('[JOB] getTodayCardPromo starts');
+                SSG.getTodayCardPromoInfo(function (err, cardPromoList) {
+                    console.log(cardPromoList);
+                    _.each(cardPromoList, function (cardPromo) {
+                        db.saveCardPromo(cardPromo, function(err, response) {
+                            if (err) console.error('err: ' + JSON.stringify(err));
+                            else console.log('response: ' + JSON.string(response));
+                        });
+                    });
+                });
+            },
+            start: false,
+            timeZone: 'Asia/Seoul'
+        });
+    },
+
+    sendCardPromoMessageJob: function() {
+
+    },
+
+    test: function() {
+        SSG.getTodayCardPromoInfo(function (err, cardPromoList) {
+                _.each(cardPromoList, function (cardPromo) {
+                db.saveCardPromo(cardPromo, function(err, response) {
+                    if (err) console.error('err: ' + JSON.stringify(err));
+                    else console.log('response: ' + JSON.string(response));
+                });
+            });
+        });
+    }
+};
+
+//new CronJob('00 00 7-17 * * *', function() {
+//}, null, true, null, null);
+Job.test();
+
+module.exports = Job;
+
--- ssg.js
+++ ssg.js
@@ -3,37 +3,31 @@ var _ = require('underscore'),
     x = Xray(),
     Telegram = require('./telegram');
 
+
 var SSG = {
     getTodayCardPromoInfo: function(fn) {
         var self = this;
         x('http://www.ssg.com', '#bnBlistarea .bn_blist li', [{
             link: 'a@href',
-            name: 'img@alt',
+            promoName: 'img@alt',
             imgUrl: 'img@src'
         }])(function(err, cardPromoList) {
             if (err)
                 fn(err);
             else
-                console.log(cardPromoList);
                 fn(null, self.attachMessage(cardPromoList));
         });
     },
     attachMessage: function(cardPromoList) {
 
         return _.map(cardPromoList, function(cardPromo) {
+            cardPromo.cardName = cardPromo.promoName.substring(0, 4);
             cardPromo.link = cardPromo.link.substring(0, cardPromo.link.indexOf('&tlid'));
-            cardPromo.message = '오늘은 ' + cardPromo.name + ' 행사일입니다. ' + cardPromo.link;
+            cardPromo.message = '오늘은 ' + cardPromo.promoName + ' 행사일입니다. ' + cardPromo.link;
 
             return cardPromo;
         });
-
     }
 };
-/*
-SSG.getTodayCardPromoInfo(function(err, cardPromoList) {
-    _.each(cardPromoList, function(cardPromo) {
-        Telegram.sendMessage(49397784, cardPromo.message);
-    });
-});
-*/
+
 module.exports = SSG;
